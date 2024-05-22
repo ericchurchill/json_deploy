a39b5402607e005bab2a72df9cfa2ac4ba562fdd
ericchurchill <m.ericchurchill@gmail.com>
2024-05-22 17:03:02 +0530

    corsss

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 8080; //  chose port from here like 8080, 3001
-const cors = require('cors')
+const cors = require (‘cors’)
 
+app.use(cors({
+      origin: “https://auction-web-10k5.onrender.com”,
+    }))
 server.use(middlewares);
 server.use(router);
 server.use(cors());

e003c2e8e852290c8b1a26c55833ae40fdbec4c1
ericchurchill <m.ericchurchill@gmail.com>
2024-05-22 13:40:24 +0530

    cors

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 8080; //  chose port from here like 8080, 3001
+const cors = require('cors')
 
 server.use(middlewares);
 server.use(router);
-
+server.use(cors());
 server.listen(port);
